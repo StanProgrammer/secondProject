2647f8e8755dda7f7f5b8b75f077091e4f707a77
AtibProgrammer <atibkhan392@gmail.com>
2023-02-16 14:46:56 +0530

    Done With Async await

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var t1 = document.getElementById('table1')
 var t2 = document.getElementById('table2')
 var t3 = document.getElementById('table3')
 var myform = document.getElementById('my-form')
-myform.addEventListener('submit', OnSubmit)
-function OnSubmit(e) {
-    a = Math.floor(Math.random() * 1000000)
+myform.addEventListener('submit', async()=>{
+    try{
+        a = Math.floor(Math.random() * 1000000)
     let myObj = {
         id: a,
         price: price1.value,
@@ -16,57 +16,58 @@ function OnSubmit(e) {
         table: table.value,
 
     }
-    e.preventDefault();
-    axios
-        .post('https://crudcrud.com/api/52b6fb418fa54f1cac8ec4c344fa3f76/orders', { myObj })
-        .then((res) => {
-            showAll(myObj,res.data._id)
-        })
-        .catch((err) => {
-            document.body.innerHTML = document.body.innerHTML + "<h4>Something went Wrong<h4>"
-            console.log(err)
-        })
+    const post1=await axios
+        .post('https://crudcrud.com/api/e3b90910bc1b4a899f82da32008f4156/orders', { myObj })
+        showAll(myObj, post1.data._id)
     price1.value = '';
     detail.value = '';
     table.value = '';
-}
-window.addEventListener('DOMContentLoaded', (event) => {
-    function getTodos() {
-        axios
-            .get('https://crudcrud.com/api/52b6fb418fa54f1cac8ec4c344fa3f76/orders')
-            .then(res => show(res))
-            .catch(err => console.log(err))
     }
-    getTodos()
+    catch(err){
+        console.log(err)
+        throw new Error()
+    }
 })
-function show(res) {
+
+window.addEventListener('DOMContentLoaded', async (event) => {
+    try{
+    const res=await axios.get('https://crudcrud.com/api/e3b90910bc1b4a899f82da32008f4156/orders')
     for (i = 0; i < res.data.length; i++) {
         showAll(res.data[i].myObj, res.data[i]._id)
     }
-}
-function showAll(res,id) {
+    }
+    catch(err){
+        console.log(err)
+        throw new Error()
+    }           
+})
+
+async function showAll(res, id) {
+    try{
     let li = document.createElement('li')
     let btn = document.createElement('button')
     li.className = 'card mt-3'
     btn.className = 'form-control bg-info'
     btn.value = 'delete'
-    li.id=res.id
-    a=li.id
-    btn.onclick = function () { 
-        if(confirm('Are you sure?')){
-        var e=document.getElementById(res.id)
-        var ul=e.parentElement
-        axios
-                        .delete(`https://crudcrud.com/api/52b6fb418fa54f1cac8ec4c344fa3f76/orders/${id}`)
-                        .then((res) => {
-                            ul.removeChild(e)
-                        })
-                        .catch(err => alert('Not Found'))
-    };
-    }
-
+    li.id = res.id
+    a = li.id
+    btn.onclick = async ()=> {
+        try{
+            if (confirm('Are you sure?')) {
+                var e = document.getElementById(res.id)
+                var ul = e.parentElement
+                const delete1=await axios
+                    .delete(`https://crudcrud.com/api/e3b90910bc1b4a899f82da32008f4156/orders/${id}`)
+                    ul.removeChild(e)
+            };
+        }
+        catch(err){
+            console.log(err)
+            throw new Error()
+        }
+        };
     btn.appendChild(document.createTextNode(`Delete`))
-    li.innerHTML=`<table class="table">
+    li.innerHTML = `<table class="table">
     
     <tbody>
       <tr>
@@ -92,5 +93,10 @@ function showAll(res,id) {
     }
     else {
         t3.appendChild(li)
+    }
+    }
+    catch(err){
+        console.log(err)
+        throw new Error()
     }
 }
